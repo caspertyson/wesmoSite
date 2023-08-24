dd3b4b962b660602933d201d07eb24091a711a48
Casper Tyson <caspertyson99@gmail.com>
2023-08-25 09:24:58 +1200

    navbar ui for phone

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typewriterElem = document.getElementById('typewriter');
-const words = ['Upcoming Engineers ', 'Students ', 'University '];
+const words = ['Engineers ', 'Students ', 'University '];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
@@ -27,5 +27,57 @@ function type() {
     let timeout = isDeleting ? 100 : 200; // Adjust typing and deleting speed here
     setTimeout(type, timeout);
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-left .nav-links');
+    const body = document.querySelector('body');
+
+    // Create a div to cover the screen
+    const navCover = document.createElement('div');
+    navCover.className = 'nav-cover';
+
+    // Copy the navLinks to navCover
+    navCover.appendChild(navLinks.cloneNode(true));
+
+    // Append the navCover to the body
+    body.appendChild(navCover);
+
+    // Ensure click outside the hamburger when menu is open closes the menu
+    navCover.addEventListener('click', function(event) {
+        if(event.target === navCover) {
+            closeMenu();
+        }
+    });
+
+    hamburgerMenu.addEventListener('click', function() {
+        // Toggle active class to animate nav cover
+        navCover.classList.toggle('active');
+
+        // Toggle the hamburger to an X
+        toggleMenu();
+    });
+
+    function closeMenu() {
+        navCover.classList.remove('active');
+        toggleMenu();
+    }
+
+    function toggleMenu() {
+        hamburgerMenu.classList.toggle('toggle');
+
+        // Append lines to convert hamburger to X
+        let lines = document.querySelectorAll('.hamburger-menu .line');
+        if(hamburgerMenu.classList.contains('toggle')) {
+            lines[0].style.transform = 'rotate(45deg) translate(5px, 5px)';
+            lines[1].style.opacity = '0';
+            lines[2].style.transform = 'rotate(-45deg) translate(5px, -5px)';
+        } else {
+            lines.forEach(line => {
+                line.style.transform = 'none';
+                line.style.opacity = '1';
+            });
+        }
+    }
+});
 
 window.onload = type;  // Start the typewriter effect once the page is loaded
